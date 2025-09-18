b816b6fbc039be98d18108802b0a0fd9d8d2e70b
Biswarup Debnath <46041035+BiswarupDebnath@users.noreply.github.com>
2025-09-18 22:49:09 +0530

    Update utils.py

--- 2_model_training/utils.py
+++ 2_model_training/utils.py
@@ -8,7 +8,7 @@ from PIL import Image
 from torchvision.ops import box_convert, box_iou
 
 # Initialise data paths
-DATASET_PATH = '/Users/biswarupdebnath/Desktop/laptop_backup/laptop_Documents/personal_documents/job_prep/bosch/assignment_data_bdd/'
+DATASET_PATH = '/home/jovyan/assignment_data_bdd/'
 
 IMAGES_PATH = osp.join(DATASET_PATH, 'bdd100k_images_100k/bdd100k/images/100k')
 LABELS_PATH = osp.join(DATASET_PATH, 'bdd100k_labels_release/bdd100k/labels')
