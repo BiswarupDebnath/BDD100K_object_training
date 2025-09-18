9d9a3f91aef2c86fedfb5c2c47799986250727fc
Biswarup Debnath <46041035+BiswarupDebnath@users.noreply.github.com>
2025-09-19 01:33:28 +0530

    Add files via upload

--- 3_evaluation_and_visualisation/model.py
+++ 3_evaluation_and_visualisation/model.py
@@ -1,7 +1,7 @@
+import warnings
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import warnings
 
 
 class ConvBlock(nn.Module):
@@ -16,10 +16,12 @@ class ConvBlock(nn.Module):
 
 
 class BottleneckBlock(nn.Module):
-    """A bottleneck layer with optional shortcut and group convolution for efficient feature extraction."""
+    """A bottleneck layer with optional shortcut and group convolution for efficient feature
+    extraction."""
 
     def __init__(self, c1, c2, shortcut=True, e=0.5):
-        """Initializes a standard bottleneck layer with optional shortcut supporting channel expansion."""
+        """Initializes a standard bottleneck layer with optional shortcut supporting channel
+        expansion."""
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = ConvBlock(c1, c_, 1, 1, 0)
@@ -27,38 +29,41 @@ class BottleneckBlock(nn.Module):
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
-        """Processes input through two convolutions, optionally adds shortcut if channel dimensions match; input is a
-        tensor.
+        """Processes input through two convolutions, optionally adds shortcut if channel
+        dimensions match; input is a tensor.
         """
         return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
 
 
 class C3(nn.Module):
-    """Implements a CSP Bottleneck module with three convolutions for enhanced feature extraction in neural networks."""
+    """Implements a CSP Bottleneck module with three convolutions for enhanced feature
+    extraction in neural networks."""
 
     def __init__(self, c1, c2, n=1, shortcut=True, e=0.5):
-        """Initializes C3 module with options for channel count, bottleneck repetition, shortcut usage, group
-        convolutions, and expansion.
+        """Initializes C3 module with options for channel count, bottleneck repetition,
+        shortcut usage, group convolutions, and expansion.
         """
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = ConvBlock(c1, c_, 1, 1)
         self.cv2 = ConvBlock(c1, c_, 1, 1)
-        self.cv3 = ConvBlock(2 * c_, c2, 1, 1)  # optional act=FReLU(c2)
+        self.cv3 = ConvBlock(2 * c_, c2, 1, 1)
         self.m = nn.Sequential(*(BottleneckBlock(c_, c_, shortcut, e=1.0) for _ in range(n)))
 
     def forward(self, x):
-        """Performs forward propagation using concatenated outputs from two convolutions and a Bottleneck sequence."""
+        """Performs forward propagation using concatenated outputs from two convolutions and a
+        Bottleneck sequence."""
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
 
 
 class SPPF(nn.Module):
-    """Implements a fast Spatial Pyramid Pooling (SPPF) layer for efficient feature extraction in YOLOv5 models."""
+    """Implements a fast Spatial Pyramid Pooling (SPPF) layer for efficient feature extraction in
+    CustomYOLOv5 models."""
 
     def __init__(self, c1, c2, k=5):
         """
-        Initializes YOLOv5 SPPF layer with given channels and kernel size for YOLOv5 model, combining convolution and
-        max pooling.
+        Initializes SPPF layer with given channels and kernel size for model,
+        combining convolution and max pooling.
 
         Equivalent to SPP(k=(5, 9, 13)).
         """
@@ -69,21 +74,23 @@ class SPPF(nn.Module):
         self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
 
     def forward(self, x):
-        """Processes input through a series of convolutions and max pooling operations for feature extraction."""
+        """Processes input through a series of convolutions and max pooling operations for
+        feature extraction."""
         x = self.cv1(x)
         with warnings.catch_warnings():
-            warnings.simplefilter("ignore")  # suppress torch 1.9.0 max_pool2d() warning
+            warnings.simplefilter("ignore")
             y1 = self.m(x)
             y2 = self.m(y1)
             return self.cv2(torch.cat((x, y1, y2, self.m(y2)), 1))
 
 
 class CustomYOLOv5(nn.Module):
-    def __init__(self, num_classes=10, num_anchors=1, anchors=[[8, 8]]):
+    def __init__(self, num_classes=10, num_anchors=1, anchors=None):
         super().__init__()
         # input = 384 x 640 x 3
 
         self.stride = 16
+        anchors = [[8, 8]] if anchors is None else anchors
         self.anchors = torch.tensor(anchors)
 
         self.nc = num_classes
@@ -105,7 +112,7 @@ class CustomYOLOv5(nn.Module):
 
         self.neck = nn.Sequential(
             C3(256, 256, 1),
-            ConvBlock(256, self.na * (self.nc + 5), 1, 1, 0)  # 24 x 40 x 15
+            ConvBlock(256, self.na * (self.nc + 5), 1, 1, 0)
         )
 
     def head(self, x):
@@ -113,8 +120,8 @@ class CustomYOLOv5(nn.Module):
         x = x.view(bs, self.na, (self.nc + 5), h, w).permute(0, 1, 3, 4, 2).contiguous()
 
         xy, wh, p, conf = x.sigmoid().split((2, 2, 1, self.nc), dim=4)
-        xy = (xy * 2 + self.grid) * self.stride  # xy
-        wh = (wh * 2) ** 2 * self.anchor_grid  # wh
+        xy = (xy * 2 + self.grid) * self.stride
+        wh = (wh * 2) ** 2 * self.anchor_grid
         conf = F.softmax(conf, dim=4)
         y = torch.cat((xy, wh, p, conf), dim=4)
         return y
